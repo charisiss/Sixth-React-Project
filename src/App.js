7cbaf08686ca8104f77ad49b0084d83368c5a58d
xsxsam <54262610+xsxsam@users.noreply.github.com>
2022-09-19 19:02:03 +0300

    Custom Counter Hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import BackwardCounter from './components/BackwardCounter';
-import ForwardCounter from './components/ForwardCounter';
+import React from "react";
+import BackwardCounter from "./components/BackwardCounter";
+import ForwardCounter from "./components/ForwardCounter";
 
 function App() {
   return (
     <React.Fragment>
+      <p style={{ textAlign: "center", color: "white" }}>
+        This is just a simple counter, to practice on custom hooks
+      </p>
+
       <ForwardCounter />
       <BackwardCounter />
     </React.Fragment>
